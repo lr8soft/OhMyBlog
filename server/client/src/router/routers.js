52c5fa01a61985d24f8b238cfec3cab1bf29f879
LT_lrsoft <lrsoft@hotmail.com>
2023-05-29 17:13:46 +0800

    v0.12

--- server/client/src/router/routers.js
+++ server/client/src/router/routers.js
@@ -2,14 +2,17 @@
 import LoginComp from "@/components/LoginComp.vue";
 import MainComp from "@/components/MainComp.vue";
 import RegistComp from "@/components/RegistComp.vue";
+import PageNotFoundComp from "@/components/PageNotFoundComp.vue";
+import LogoutComp from "@/components/LogoutComp.vue";
 
 
 const routes = [
     { path:'/', component: MainComp },
     { path:'/login', component: LoginComp },
     { path:'/regist', component: RegistComp },
+    { path:'/logout', component: LogoutComp },
     // 最后匹配的就是404页面
-    //{ path:'/:pathMatch(.*)', component: PageNotFoundComp }
+    { path:'/:pathMatch(.*)', component: PageNotFoundComp }
 ]
 
 export default routes
